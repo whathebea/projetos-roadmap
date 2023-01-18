37c436f275dea91722459450210ea13e8234bb9a
whathebea <bea.graciast@gmail.com>
2023-01-17 22:01:45 -0300

    Adicionado primeiro projeto

---
+++ projeto-calc/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // implementar aqui mensagem inicial
+
+        // pegar input do usuario
+
+        // implementar switch case para cada operacao
+
+        // implementar mensagem de saida
+
+    }
+}
---
+++ projeto-calc/src/main/java/org/example/Operacoes.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class Operacoes {
+
+    // depois de implementar as operacoes, retire o return 0 e os comentarios
+
+    public static int soma() {
+        // sua implementacao
+        return 0;
+    }
+
+    public static int subtracao() {
+        // sua implementacao
+        return 0;
+    }
+
+    public static int multiplicacao() {
+        // sua implementacao
+        return 0;
+    }
+
+    public static int divisao() {
+        // sua implementacao
+        return 0;
+    }
+
+}
