df4e28a7d49747420d39471e8406b21c81d5a703
whathebea <bea.graciast@gmail.com>
2023-01-17 22:08:31 -0300

    Adicionado segundo projeto

---
+++ adivinhar-numero/src/main/java/org/example/AdivinharNumero.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class AdivinharNumero {
+
+    public static void adivinhaNumero() {
+        // mensagem inicial
+
+        // determinar quantas tentativas o usuario tera
+
+        // input do usuario
+
+        // gerar numero aleatorio
+
+        // comparar numero aleatorio com input do usuario (loop)
+    }
+}
---
+++ adivinhar-numero/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import static org.example.AdivinharNumero.adivinhaNumero;
+
+public class Main {
+    public static void main(String[] args) {
+        adivinhaNumero();
+    }
+}
